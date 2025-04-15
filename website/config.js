f120e1a8d04bc25189274e755063528793cc3430
Akinyi83 <133520053+Akinyi83@users.noreply.github.com>
2025-04-15 17:30:49 +0300

    Add files via upload
    website files

---
+++ website/config.js
@@ -0,0 +1,4 @@
+window.COFFEE_CONFIG = {
+	API_GW_BASE_URL_STR: "https://fxrlnt8rqc.execute-api.us-east-1.amazonaws.com/prod",
+	COGNITO_LOGIN_BASE_URL_STR: "https://cafe-c58885a1045928l9959998t1w646423462326.auth.us-east-1.amazoncognito.com/login?client_id=3gudoanm0fhfq1v0jc6scmetfd&response_type=token&scope=email+openid&redirect_uri=https://d2qrabwpzgq09c.cloudfront.net/callback.html"
+};
